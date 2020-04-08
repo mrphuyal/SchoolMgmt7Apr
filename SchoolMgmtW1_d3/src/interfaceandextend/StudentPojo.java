8db44c080b3e20c9664262b5ce495eb17292d4b3
Sudip Phuyal <mr.phuyal@gmail.com>
2020-04-08 22:34:27 +0545

    ,

---
+++ SchoolMgmtW1_d3/src/interfaceandextend/StudentPojo.java
@@ -0,0 +1,26 @@
+package interfaceandextend;
+
+public class StudentPojo extends CommonPojo{
+	private String firstName;
+	private String lastName;
+	private String phNumber;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhNumber() {
+		return phNumber;
+	}
+	public void setPhNumber(String phNumber) {
+		this.phNumber = phNumber;
+	}
+
+}
